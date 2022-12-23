d22d55552edc8c25c29675a701408c38fb2be525
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-23 19:54:24 +0530

    calling the methods

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CollectionDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to c# collection!");
+            Demo.doListDemo();
+            Demo.doStackDemo();
+            Demo.doDictionaryDemo();
+            Demo.doQueueDemo();
+            Demo.doSetDemo();
+            Console.ReadKey();
+        }
+    }
+}
